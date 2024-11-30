b144f20b59abf8fef59850f227d7cce7cd30f904
souradipdev <22052939@kiit.ac.in>
2024-12-01 03:20:38 +0530

    Added ProductPage object

--- src/main/java/com/saucedemo/ProductPage.java
+++ src/main/java/com/saucedemo/ProductPage.java
@@ -1,7 +1,12 @@
 package com.saucedemo;
 
 import com.base.BasePage;
+import org.openqa.selenium.By;
 
 public class ProductPage extends BasePage {
+  private By productHeader = By.xpath("");
 
+  public boolean isProductHeaderDisplayed(){
+    return find(productHeader).isDisplayed();
+  }
 }
