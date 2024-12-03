6b8819ee277359860e14ebe25938a72eac1e0419
souradipdev <22052939@kiit.ac.in>
2024-12-03 17:09:04 +0530

    Added ElementPage in element package

--- src/main/java/com/demoqa/pages/elements/WebTablesPage.java
+++ src/main/java/com/demoqa/pages/elements/WebTablesPage.java
@@ -1,7 +1,32 @@
 package com.demoqa.pages.elements;
 
-import com.demoqa.pages.HomePage;
+import org.openqa.selenium.By;
 
 public class WebTablesPage extends ElementPage {
 
+  By editAge = By.id("age");
+  By editSalary = By.id("salary");
+  By submit = By.id("submit");
+
+  public void clickEdit(String email) {
+    By edit = By.xpath("//div[text()=' " + email + "']//following::span");
+    clickUsingLocator(edit);
+  }
+
+  public void setEditAge(String age) {
+    setKeys(editAge, age);
+  }
+
+  public void setEditSalary(String salary) {
+    setKeys(editSalary, salary);
+  }
+
+  public void submit() {
+    clickUsingLocator(submit);
+  }
+
+  public String getEditedAge(String email) {
+    By editedAge = By.xpath("//div[text()=' " + email + " ']//preceding::div[1]");
+    return findUsingLocator(editedAge).getText();
+  }
 }
