bb9cfaf62bee09f5cc2666eeceb913085d45944b
souradipdev <22052939@kiit.ac.in>
2024-12-05 19:56:55 +0530

    Added screenshot feature during failed test

--- src/test/java/part3/com/demoqa/test/base/BaseTest.java
+++ src/test/java/part3/com/demoqa/test/base/BaseTest.java
@@ -1,10 +1,18 @@
 package part3.com.demoqa.test.base;
 
 import com.base.BasePage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.FileHandler;
 
 public class BaseTest {
   private WebDriver driver;
@@ -27,8 +35,37 @@ public class BaseTest {
     basePage.setDriver(driver);
   }
 
+  @AfterMethod
+  public void takeFailedResultScreenshot(ITestResult testResult) {
+    if (ITestResult.FAILURE == testResult.getStatus()) {
+      try {
+        if (driver == null) {
+          throw new IllegalStateException("WebDriver is not initialized.");
+        }
+
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        File screenshotDir = new File(System.getProperty("user.dir") + "/resources/screenshots/");
+        if (!screenshotDir.exists()) {
+          screenshotDir.mkdirs();
+        }
+
+        String timestamp = java.time.LocalDateTime.now().toString().replace(":", "-");
+        File destination = new File(screenshotDir, "(" + timestamp + ") " + testResult.getName() + ".png");
+
+        Files.copy(source.toPath(), destination.toPath());
+        System.out.println("Screenshot saved at: " + destination.getAbsolutePath());
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("Failed to take screenshot: " + e.getMessage(), e);
+      }
+    }
+  }
+
+
   @AfterClass
   public void tearDown() {
-//    driver.quit();
+    driver.quit();
   }
 }
