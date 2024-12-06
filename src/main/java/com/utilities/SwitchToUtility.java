ebceab01bcdd942a0175331823905a7824ceacef
souradipdev <22052939@kiit.ac.in>
2024-12-07 01:50:13 +0530

    Added frame switching utilities

--- src/main/java/com/utilities/SwitchToUtility.java
+++ src/main/java/com/utilities/SwitchToUtility.java
@@ -1,6 +1,7 @@
 package com.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SwitchToUtility extends Utility {
 
@@ -23,5 +24,22 @@ public class SwitchToUtility extends Utility {
   public static void sendAlertKeys(String text) {
     switchTo().alert().sendKeys(text);
   }
+
+  public static void switchToFrameString(String nameOrId){
+    switchTo().frame(nameOrId);
+  }
+
+  public static void switchToDefaultContext(){
+    switchTo().defaultContent();
+  }
+
+  public static void switchToFrameIndex( int index){
+    switchTo().frame(index);
+  }
+
+  public static void switchToFrameElement(WebElement element){
+    switchTo().frame(element);
+  }
+  
 
 }
